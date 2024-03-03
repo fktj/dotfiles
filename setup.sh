9f2b891b06fc224408a2078b67ca847ebdfa9095
fktj <161435477+fktj@users.noreply.github.com>
2024-03-03 22:13:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-
+echo "######### Setup log #########" >> setuplog.txt
 echo "Please enter your email:"
 read user_email
-echo "Please enter your username:"
-read user_name
-echo "$user_email"
-echo "$user_name"
 
-# Get shell name
-shell_name=$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)
+if [ -n "$SUDO_USER" ]; then
+    user_name=$SUDO_USER
+else
+    user_name=$(whoami)
+fi
 
+if [ -n "$SUDO_USER" ]; then
+    user_home=$(getent passwd $SUDO_USER | cut -d: -f6)
+else
+    user_home=$user_home
+fi
 
-# Function to install a package if it's not already installed
+shell_name=$(basename $SHELL)
+
+echo "Ran as: $user_name" >> setuplog.txt
+echo "User home: $user_home" >> setuplog.txt
+echo "Shell name: $shell_name" >> setuplog.txt
+echo "---------------------------------" >> setuplog.txt
+
 install_package() {
     if ! command -v $1 &> /dev/null
     then
@@ -22,45 +32,33 @@ install_package() {
     fi
 }
 
-# Update package lists
 sudo apt update -y
-
-# Upgrade packages and remove unnecessary packages
 sudo apt upgrade -y && sudo apt autoremove -y
 
-# Install basic packages
 echo "Do you want to setup basics? (yes/no)"
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "Installing basic packages"
 
-    # Array of packages to install
     packages=(  "unzip" \
                 "zsh" \
                 "gh" \
-                "gnupg" \
-                "openssh-client" \
                 "git" \
                 "python3-pip" \
                 "pipx" \
                 "ca-certificates" \
                 "curl" \
-                "apt-transport-https" \
-                "lsb-release" \
-                "direnv"
+                "lsb-release" 
             )
 
-    # Loop over the array and install each package
     for package in "${packages[@]}"
     do
         install_package $package
     done
 
-
     # Check if certain commands are installed, if not install them
     commands=( "databricks" "az" "git-credential-manager" "mcfly" "zoxide" "zsh" "starship" )
-
 
     for command in "${commands[@]}"
     do
@@ -68,49 +66,43 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         then
             echo "Installing $command..."
             case $command in
+                "zsh")
+                    if [ ! -d "$user_home/.oh-my-zsh" ]
+                    then
+                        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+                        chsh -s /bin/zsh $user_name
+                        sudo apt install fzf -y && sudo apt install zsh-autosuggestions -y
+                    fi
+                    ;;
                 "databricks")
                     curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sudo sh
                     ;;
                 "az")
-                    # Azure CLI installation script
-                    curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+                    curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpgv --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
                     AZ_REPO=$(lsb_release -cs)
-                    # might be an issue here
                     echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/trusted.gpg.d/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
                     sudo apt update
                     sudo apt install azure-cli -y
-                    az_version=$(az --version)
                     ;;
                 "git-credential-manager")
-                    # Insert Git Credential Manager installation script here
-                    sudo dpkg --print-architecture
                     wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb && sudo dpkg -i /tmp/gcmcore.deb
-                    git_cred_manager_version=$(git-credential-manager --version)
                     ;;
                 "mcfly")
-                    curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly --to ~/.local/bin
+                    curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly --to $user_home/.local/bin
                     ;;
                 "zoxide")
                     curl -sS https://webinstall.dev/zoxide | bash
                     ;;
-                "zsh")
-                    if [ ! -d "$HOME/.oh-my-zsh" ]
-                    then
-                        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-                        chsh -s /bin/bash $user_name
-                        sudo apt install fzf -y && sudo apt install zsh-autosuggestions -y
-                    fi
-                    ;;
                 "starship")
-                    mkdir -p ~/.local/share/fonts/
+                    mkdir -p $user_home/.local/share/fonts/
                     for type in Bold Light Medium Regular Retina; 
                     do 
-                        wget -nc -O ~/.local/share/fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; 
+                        wget -nc -O $user_home/.local/share/fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; 
                     done
-                    fc-cache -f ~/.local/share/fonts/
-                    mkdir -p ~/.local/bin
-                    RUNZSH=no sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force --bin-dir ~/.local/bin
-                    export PATH="~/.local/bin:$PATH"
+                    fc-cache -f $user_home/.local/share/fonts/
+                    mkdir -p $user_home/.local/bin
+                    RUNZSH=no sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force --bin-dir $user_home/.local/bin
+                    export PATH="$user_home/.local/bin:$PATH"
                     ;;
             esac
         else
@@ -118,54 +110,49 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         fi
     done
 
-
     # Ensure dotfiles are downloaded.
-    mkdir -p ~/.dotfiles
-    wget -N -P ~/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/starship.toml"
-    wget -N -P ~/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.commonrc"
-    wget -N -P ~/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.zshrc"
-    wget -N -P ~/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.bashrc"
-
+    mkdir -p $user_home/.dotfiles || echo "Failed to create directory $user_home/.dotfiles"
+    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/starship.toml"
+    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.commonrc"
+    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.zshrc"
+    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.bashrc"
 
     # Ensure dotfiles are symlinked.
     echo "Symlinking dotfiles"
-    sleep 2
     case $shell_name in
         *"zsh"* )
-            rm -f ~/.zshrc
-            ln -s ~/.dotfiles/.zshrc ~/.zshrc
+            rm -f $user_home/.zshrc
+            ln -s $user_home/.dotfiles/.zshrc $user_home/.zshrc
             ;;
         *"bash"* )
-            rm -f ~/.bashrc
-            ln -s ~/.dotfiles/.bashrc ~/.bashrc
+            rm -f $user_home/.bashrc
+            ln -s $user_home/.dotfiles/.bashrc $user_home/.bashrc
             ;;
     esac
-    mkdir -p ~/.config
-    rm -f ~/.config/starship.toml
-    ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
+    mkdir -p $user_home/.config || echo "Failed to create directory $user_home/.config"
+    rm -f $user_home/.config/starship.toml
+    ln -s $user_home/.dotfiles/starship.toml $user_home/.config/starship.toml
     
     # Source dotfiles.
     case $shell_name in
         *"zsh"* )
-            test -e ~/.zshrc && . ~/.zshrc
+            test -e $user_home/.zshrc && . $user_home/.zshrc
             ;;
         *"bash"* )
-            test -e ~/.bashrc && . ~/.bashrc
+            test -e $user_home/.bashrc && . $user_home/.bashrc
             ;;
     esac
 
-
     # Set pipx path and add poetry
     pipx ensurepath
-    source ~/.bashrc || source ~/.zshrc
+    source $user_home/.bashrc || source $user_home/.zshrc
     pipx_version=$(pipx --version)
     pipx install poetry
-    source ~/.bashrc || source ~/.zshrc
+    source $user_home/.bashrc || source $user_home/.zshrc
     pipx upgrade poetry
-    export PATH="$HOME/.local/bin:$PATH"
+    export PATH="$user_home/.local/bin:$PATH"
     poetry_version=$(poetry --version)
  
-
     # Set global git config
     echo "Do you want to set global git config? (yes/no)"
     read answer
@@ -177,14 +164,37 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         git config --global credential.helper "$(which git-credential-manager)"
     fi
 
-    # Configure ssh connection
-    ssh-keygen -t rsa -b 4096 -C $user_email
-    # Start the ssh-agent in the background
-    eval "$(ssh-agent -s)"
-    # Add your SSH private key to the ssh-agent
-    ssh-add ~/.ssh/id_rsa
-    echo "######### Add this SSH key to your azure devops account  #########"
-    cat ~/.ssh/id_rsa.pub
+    unzip_version="unsip: $(unzip -v | head -n 1)"
+    zsh_version=$(zsh --version)
+    gh_version=$(gh --version | head -n 1)
+    git_version=$(git --version)
+    python3_version=$(python3 --version)
+    python3_pip_version=$(pip3 --version)
+    ca_certificates_version="ca-certificates: $(apt-cache policy ca-certificates | head -n 3)"
+    curl_version=$(curl --version | head -n 1 | awk '{print $1, $2, $3}')
+    lsb_release_version="lsb release: $(lsb_release -a 2>/dev/null | grep Description | cut -f 2- -d ':')"
+    databricks_version=$(databricks --version)
+    az_version=$(az --version | head -n 1)
+    git_cred_manager_version="git credential manger: $(git-credential-manager --version)"
+    pipx_version="pipx: $(pipx --version)"
+    poetry_version=$(poetry --version)
+    git_config=$(git config --global --list)
+
+    echo "$unzip_version" >> setuplog.txt
+    echo "$zsh_version" >> setuplog.txt
+    echo "$gh_version" >> setuplog.txt
+    echo "$git_version" >> setuplog.txt
+    echo "$python3_version" >> setuplog.txt
+    echo "$python3_pip_version" >> setuplog.txt
+    echo "$ca_certificates_version" >> setuplog.txt
+    echo "$curl_version" >> setuplog.txt
+    echo "$lsb_release_version" >> setuplog.txt
+    echo "$databricks_version" >> setuplog.txt
+    echo "$az_version" >> setuplog.txt
+    echo "$git_cred_manager_version" >> setuplog.txt
+    echo "$pipx_version" >> setuplog.txt
+    echo "$poetry_version" >> setuplog.txt
+    echo "$git_config" >> setuplog.txt
     echo "######### Done  #########"
     echo "Remember to move or symlink any local rc to ~/.localrc"
 fi
