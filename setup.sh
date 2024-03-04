016866109bda967a2ce96ea796f67037b256707a
fktj <161435477+fktj@users.noreply.github.com>
2024-03-04 20:56:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -89,7 +89,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
                     wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb && sudo dpkg -i /tmp/gcmcore.deb
                     ;;
                 "poetry")
-                    curl -sSL https://install.python-poetry.org | sudo python3 - --prefix=/usr/local
+                    mkdir -v -f $user_home/.poetry/bin
+                    curl -sSL https://install.python-poetry.org | sudo python3 - --prefix=$user_home/.poetry/bin
                     ;;
             esac
         else
