a577eaaaf473071b9a45b6878d9f87d6d14fc2e1
fktj <161435477+fktj@users.noreply.github.com>
2024-03-04 10:55:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,8 +15,6 @@ else
     user_home=$HOME
 fi
 
-shell_name=$(basename $SHELL)
-
 echo "Ran as: $user_name" >> setuplog.txt
 echo "User home: $user_home" >> setuplog.txt
 echo "Shell name: $shell_name" >> setuplog.txt
@@ -60,7 +58,7 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     done
 
     # Check if certain commands are installed, if not install them
-    commands=( "databricks" "az" "git-credential-manager" "mcfly" "zoxide" "zsh" "starship" )
+    commands=("zsh" "databricks" "az" "git-credential-manager" "poetry")
 
     for command in "${commands[@]}"
     do
@@ -89,68 +87,14 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
                 "git-credential-manager")
                     wget "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.deb" -O /tmp/gcmcore.deb && sudo dpkg -i /tmp/gcmcore.deb
                     ;;
-                "mcfly")
-                    curl -LSfs https://raw.githubusercontent.com/cantino/mcfly/master/ci/install.sh | sh -s -- --git cantino/mcfly --to $user_home/.local/bin
-                    ;;
-                "zoxide")
-                    curl -sS https://webinstall.dev/zoxide | bash
-                    ;;
-                "starship")
-                    mkdir -p $user_home/.local/share/fonts/
-                    for type in Bold Light Medium Regular Retina; 
-                    do 
-                        wget -nc -O $user_home/.local/share/fonts//FiraCode-$type.ttf "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/$type/complete/Fira%20Code%20$type%20Nerd%20Font%20Complete.ttf?raw=true"; 
-                    done
-                    fc-cache -f $user_home/.local/share/fonts/
-                    mkdir -p $user_home/.local/bin
-                    RUNZSH=no sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --force --bin-dir $user_home/.local/bin
-                    export PATH="$user_home/.local/bin:$PATH"
+                "poetry")
+                    curl -sSL https://install.python-poetry.org | python3 -
                     ;;
             esac
         else
             echo "$command is already installed."
         fi
     done
-
-    # Ensure dotfiles are downloaded.
-    mkdir -p $user_home/.dotfiles || echo "---->Failed to create directory $user_home/.dotfiles" >> setuplog.txt
-    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/starship.toml"
-    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.commonrc"
-    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.zshrc"
-    wget -N -P $user_home/.dotfiles/ "https://raw.githubusercontent.com/fktj/dotfiles/main/.dotfiles/.bashrc"
-
-    # Ensure dotfiles are symlinked.
-    echo "Symlinking dotfiles"
-    case $shell_name in
-        *"zsh"* )
-            rm -f $user_home/.zshrc
-            ln -s $user_home/.dotfiles/.zshrc $user_home/.zshrc
-            ;;
-        *"bash"* )
-            rm -f $user_home/.bashrc
-            ln -s $user_home/.dotfiles/.bashrc $user_home/.bashrc
-            ;;
-    esac
-    mkdir -p $user_home/.config || echo "Failed to create directory $user_home/.config"
-    rm -f $user_home/.config/starship.toml
-    ln -s $user_home/.dotfiles/starship.toml $user_home/.config/starship.toml
-    
-    # Source dotfiles.
-    case $shell_name in
-        *"zsh"* )
-            test -e $user_home/.zshrc && . $user_home/.zshrc
-            ;;
-        *"bash"* )
-            test -e $user_home/.bashrc && . $user_home/.bashrc
-            ;;
-    esac
-
-    # Set pipx path and add poetry
-    pipx ensurepath
-    export PATH="$user_home/.local/bin:$PATH"
-    source $user_home/.bashrc || source $user_home/.zshrc
-    pipx install poetry
-    pipx upgrade poetry
  
     # Set git global config
     echo "Do you want to set global git config? (yes/no)"
@@ -168,13 +112,12 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     git_version=$(git --version)
     python3_version=$(python3 --version)
     python3_pip_version=$(pip3 --version)
-    ca_certificates_version="ca-certificates: $(apt-cache policy ca-certificates | head -n 3)"
+    ca_certificates_version="$(apt-cache policy ca-certificates | head -n 3)"
     curl_version=$(curl --version | head -n 1 | awk '{print $1, $2, $3}')
     lsb_release_version="lsb release: $(lsb_release -a 2>/dev/null | grep Description | cut -f 2- -d ':')"
     databricks_version=$(databricks --version)
     az_version=$(az --version | head -n 1)
     git_cred_manager_version="git credential manger: $(git-credential-manager --version)"
-    pipx_version="pipx: $(pipx --version)"
     poetry_version=$(poetry --version)
     git_config=$(git config --global --list)
 
@@ -190,9 +133,8 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "$databricks_version" >> setuplog.txt
     echo "$az_version" >> setuplog.txt
     echo "$git_cred_manager_version" >> setuplog.txt
-    echo "$pipx_version" >> setuplog.txt
     echo "$poetry_version" >> setuplog.txt
     echo "$git_config" >> setuplog.txt
-    echo "######### Done  #########"
-    echo "Remember to move or symlink any local rc to ~/.localrc"
+    echo "######### Done  #########" >> setuplog.txt
+    echo "Done"
 fi
